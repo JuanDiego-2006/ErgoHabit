25edf0321102aa7472e50a782de855c70fc94b54
JuanDiego-2006 <jdiegora.4567@gmail.com>
2026-06-29 14:02:48 -0600

    Integración de sensores de camara y de caminar; integración de apis

--- app/src/main/java/com/knexus/ergohabit/core/di/ModuloRed.kt
+++ app/src/main/java/com/knexus/ergohabit/core/di/ModuloRed.kt
@@ -46,7 +46,7 @@ object ModuloRed {
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://192.168.1.79:8080/") // Cambia a 10.0.2.2 si usas emulador
+            .baseUrl("http://10.220.188.134:8080/") // Cambia a 10.0.2.2 si usas emulador
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
